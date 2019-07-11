c58e52307292b33515a50e8ad6ea1ee063ec0346
nekramer <44094608+nekramer@users.noreply.github.com>
2019-07-11 10:11:17 -0400

    Update file_cleanup.sh

--- scripts/file_cleanup.sh
+++ scripts/file_cleanup.sh
@@ -6,24 +6,26 @@ samplesheet=$1
 ## outputs sample list
 samples=($(python /pine/scr/n/e/nekramer/sample_names.py --samplesheet $samplesheet | tr -d '[],'))
 #samples=($(python /proj/phanstiel_lab/software/launch_pipeline/scripts/sample_names.py --samplesheet $samplesheet | tr -d '[],'))
+username=$USER
+scrspace="/pine/scr/${username:0:1}/${username:1:1}/$username"
 
 for i in $samples; 
 	do eval i=$i;
 	## go through each sample's aligned files and gzip everything except inter_30.txt, merged_nodups.txt, and inter_30.hic
-	for filename in ./$i/aligned/*;
+	for filename in $scrspace/$i/aligned/*;
 		do
 		if [ $(basename $filename) == "inter_30.txt" ] || [ $(basename $filename) == "merged_nodups.txt" ] || [ $(basename $filename) == "inter_30.hic" ];
  			then continue
- 		elif [ -d $(basename $filename) ];
- 			then tar czvf $(basename $filename).tar.gz ./$i/aligned/$(basename $filename)
-
+ 		elif [ -d $filename ];
+ 			then tar -czvf $filename.tar.gz $filename
+			rm -r $filename
  		else
  	 		gzip $filename
  	fi
  done
 
  	## go through each sample's debug files and gzip everything	
- 	for filename in ./$i/debug/*; do gzip $filename; done
+ 	for filename in $scrspace/$i/debug/*; do gzip $filename; done
 
  	## Create sample-specific aligned and debug directories in cluster if they do not exist
 	if [ ! -d "/pine/scr/n/e/nekramer/testexport/aligned" ];
@@ -35,25 +37,22 @@ for i in $samples;
 	fi
 
 	## Move and rename desired aligned and debug files to cluster with samplename_filename format
-	for filename in ./$i/aligned/*; do mv $filename "/pine/scr/n/e/nekramer/testexport/aligned/$i_$filename"; done
-	for filename in ./$i/debug/*; do mv $filename "/pine/scr/n/e/nekramer/testexport/debug/$i_$filename"; done
+	for filename in $scrspace/$i/aligned/*; do mv $filename "/pine/scr/n/e/nekramer/testexport/aligned/$i_$filename"; done
+	for filename in $scrspace/$i/debug/*; do mv $filename "/pine/scr/n/e/nekramer/testexport/debug/$i_$filename"; done
 
 	## CLEANUP FILES ##
 
 	## Unlink all fastq files
-	r. ./$i/fastq/*
+	rm $scrspace/$i/fastq/*
 
 	## Delete empty fastq directory
-	rm -r ./$i/fastq
+	rm -r $scrspace/$i/fastq
 
-	## Unlink juicer
-	unlink ./$i/juicer
-
-	## Delete empty juicer directory
-	rm -r ./$i/juicer
+	## Delete juicer directory
+	rm -r $scrspace/$i/juicer
 
 	## Delete everything else
-	rm -r ./$i
+	rm -r $scrspace/$i
 
 
 done
