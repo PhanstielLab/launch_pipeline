3e3c4896712a3094f3487ef5adf6d4a02c5a6f12
Katie Reed <ksmetz@email.unc.edu>
2020-07-04 16:59:46 -0400

    Expanded GoogleSync to cover other data types; Added RNApipe to launch

--- scripts/googleSync/googleAPIquickstart.py
+++ scripts/googleSync/googleAPIquickstart.py
@@ -2,17 +2,31 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import sys, getopt
 
 # From "Python Quickstart" page on Google Sheets API v4 webpage
 # https://developers.google.com/sheets/api/quickstart/python?authuser=1
 
+if len(sys.argv) != 2:
+	sys.exit("Please provide a data type to upload (rna, atac, chip)")
+
+datatype = sys.argv[1]
+
+sampleRanges = {
+    'rna':'rna!A1:X',
+    'atac':'atac!A1:Z',
+    'chip':'chip!A1:Z'
+    }
+
+if datatype not in sampleRanges.keys():
+	sys.exit("Please provide a valid data type to upload (rna, atac, chip)")
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '14-j6QiyzX4oV378CgQhb6btfaaopbZTRXew1FxN1vag'
-SAMPLE_RANGE_NAME = 'rna!A1:X'
+SAMPLE_RANGE_NAME = sampleRanges[datatype]
 
 def main():
 	"""Shows basic usage of the Sheets API.
